f6142b154e8d795d92b02e759f4342512e9ade88
sandro <sandrosansan@gmail.com>
2020-10-02 11:02:00 -0300

    first release

---
+++ js/slider.js
@@ -0,0 +1,67 @@
+var slides = document.querySelectorAll('.slider-container div');
+var bar = document.querySelector('.slider-bar');
+var currentSlide = 0;
+var auto = true;
+var autoTime = 3000;
+
+function prevSlide() {
+    currentSlide--;
+    if(currentSlide < 0) currentSlide = slides.length - 2;
+    hiddenSlides();
+    showSlide();
+    clearInterval(autoPlay);
+    bar.style = 'display: none';
+}
+
+function nextSlide() {
+    currentSlide++;
+    if(currentSlide == slides.length - 1) currentSlide = 0;
+    hiddenSlides();
+    showSlide();
+    clearInterval(autoPlay);
+    bar.style = 'display: none';
+}
+
+function hiddenSlides() {
+    slides.forEach((slide) => {
+        slide.classList.remove('show');
+    })
+}
+
+function showSlide() {
+    slides[currentSlide].classList.add('show');
+}
+
+function barStatus() {
+    var widthBar = 1;
+    var idBar = setInterval(frame, 20);
+    function frame() {
+        document.getElementById('slider').addEventListener('click', function() {
+            clearInterval(idBar);
+        });
+
+        if(widthBar >= 100) {
+            clearInterval(idBar);
+        } else {
+            widthBar++;
+            bar.style.width = widthBar + '%';
+        }
+    }
+}
+
+if(auto) {
+    var autoPlay = setInterval(() => {
+        (currentSlide == slides.length - 2) ? currentSlide = 0 : currentSlide++;
+        hiddenSlides();
+        slides[currentSlide].classList.add('show');
+        barStatus();
+    }, autoTime);
+}
+
+document.getElementById('slider').addEventListener('click', function() {
+    clearInterval(autoPlay);
+});
+    
+hiddenSlides();
+showSlide();
+barStatus();
